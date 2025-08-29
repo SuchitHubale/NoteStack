84458d768921727f76d13ad036fd049029efa271
SuchitHubale <2217043@ritindia.edu>
2025-08-29 16:03:05 +0530

    Deploying Frontend + Backend

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://notestack-six.vercel.app"],
     credentials: true,
 }));
 app.use(express_1.default.json());
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://notestack-7l9j.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
